74df256a0b60721da1223f10842b51cb39d15982
Somrat Sheikh <somratsheikh426@gmail.com>
2026-05-09 18:16:31 +0600

    add new route which will fetch the post by their id individually

--- main.py
+++ main.py
@@ -32,5 +32,14 @@ def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"})
 
 @app.get("/api/post")
-def get_post():
+def get_posts():
     return posts
+
+@app.get("/api/posts/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    return {"error": "post not found"}
+
+
