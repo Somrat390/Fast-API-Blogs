bef8f08925accc1725747b681160fa9d1dc7dc51
Somrat Sheikh <somratsheikh426@email.com>
2026-05-11 17:27:59 +0600

    adding route to the post.html when they want to get individual post

--- main.py
+++ main.py
@@ -35,11 +35,11 @@ def home(request: Request):
 def get_posts():
     return posts
 
-@app.get("/api/posts/{post_id}")
-def get_post(post_id: int):
+@app.get("/posts/{post_id}", include_in_schema=False)
+def get_post(request: Request, post_id: int):
     for post in posts:
         if post.get("id") == post_id:
-            return post
+            return templates.TemplateResponse(request, "post.html", {"post": post, "title": post.get("title")})
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
 
