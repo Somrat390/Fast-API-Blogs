e4d7f098b0bbf257e384bb6985a632684e15aa05
Somrat Sheikh <somratsheikh426@email.com>
2026-05-21 13:21:59 +0600

    update get post by post id route

--- main.py
+++ main.py
@@ -151,7 +151,13 @@ def create_post(post: PostCreate, db: Annotated[Session, Depends(get_db)]):
     return new_post
 
 
-
+@app.get("/api/posts/{post_id}", response_model=PostResponse)
+def get_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return post             
 
 
 @app.exception_handler(StarletteHTTPException)
